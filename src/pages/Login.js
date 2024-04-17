a958fb8c77ccba33f8f84dd16e64bae1a0102dd9
usmaan <2021.mohdusmaan.mogal@ves.ac.in>
2024-04-17 13:21:28 +0530

    verification done

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,7 +5,8 @@ import toast from "react-hot-toast";
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
+  const [otp, setOtp] = useState("");
+  const [show, setShow] = useState(false);
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
   };
@@ -13,6 +14,25 @@ const Login = () => {
   const handlePasswordChange = (e) => {
     setPassword(e.target.value);
   };
+
+  const handleOtpChange = (e) => {
+    setOtp(e.target.value);
+  };
+
+  async function handleOtpVerification(){
+    const formData = new FormData();
+    formData.append('otp',otp);
+
+    const res = await fetch("http://localhost/REACTPWA/server/verify_otp.php",{
+      method:"POST",
+      body: formData,
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    })
+    const output = await res.json();
+    console.log(output)
+  }
 
   const handleLogin = () => {
     const formData = new FormData();
@@ -34,7 +54,8 @@ const Login = () => {
         // Store JWT token in local storage
         localStorage.setItem("token", token);
         // Redirect to home page
-        window.location.href = "/home";
+        // window.location.href = "/home";
+        setShow(true)
       })
       .catch((error) => {
         console.error("Login error: ", error);
@@ -77,6 +98,26 @@ const Login = () => {
           LOGIN
         </button>
       </form>
+      {show &&
+      <div>
+      <label htmlFor="otp" className="block font-bold">
+            One time password
+      </label>
+      <input
+            type="test"
+            name="otp"
+            value={otp}
+            onChange={handleOtpChange}
+            className="px-2 mt-1 focus:outline-none focus:ring focus:ring-blue-300 block w-full h-10 rounded-md border-2 border-gray-300"
+          />
+      <button
+          type="button"
+          onClick={handleOtpVerification}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Verify otp
+        </button>
+      </div>}
     </div>
   );
 };
